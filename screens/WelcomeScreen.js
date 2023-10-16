1ab86e93f28e803d0b81dc1940671de15452eb29
Davi Silva <davi.psilva@midway.com.br>
2023-10-16 20:14:43 -0300

    get token

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const { token } = useContext(AuthContext);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get(
+        `https://react-native-course-3544e-default-rtdb.firebaseio.com/message.json?auth=${token}`
+      );
+
+      setFetchedMessage(data);
+    })();
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+
+      <Text>
+        Message from authenticaded resources:{"\n"}
+        <Text style={styles.text}>{fetchedMessage}</Text>
+      </Text>
     </View>
   );
 }
@@ -14,13 +35,19 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
+  },
+  text: {
+    margin: 8,
+    fontWeight: "bold",
+    fontSize: 16,
+    textAlign: "center",
   },
 });
